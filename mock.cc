26a51ef85205b0366011c781f60f864ad940fe0c
Cezary Kulakowski <cezary.kulakowski@gmail.com>
2018-06-24 21:09:15 +0200

    Next part of implementation of mocking mechanism.

--- mock.cc
+++ mock.cc
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <functional>
 #include <map>
+#include <string>
+#include <utility>
 #include <vector>
 
 namespace {
@@ -29,11 +31,11 @@ std::string getNameForType() {
 template <typename T, typename ARG1>
 class MockMap {
  public:
-  std::map<T*, std::vector<void(T::*)(ARG1)>> map_;
+  std::map<T*, std::vector<std::pair<void(T::*)(ARG1), std::string>>> map_;
   ~MockMap() {
     for (const auto& vec : map_) {
-      for (const auto& mock: vec.second) {
-        std::cerr << "Expected call was not called: " << getNameForType<decltype(mock)>() << std::endl;
+      for (const auto& pair: vec.second) {
+        std::cerr << "Expected call was not called: " << pair.second << std::endl;
       }
     }
   }
@@ -42,14 +44,6 @@ class MockMap {
 template <typename T, typename S>
 MockMap<T, S> g_map;
 
-class Interface {
- public:
-  virtual void foo(int) = 0;
-  virtual void foo2(double) = 0;
-};
-
-#define MOCK_CLASS(_name_)
-
 #define MOCK_METHOD1(_name_, _type_)\
   function_traits<std::function<_type_>>::result_type _name_(\
       function_traits<std::function<_type_>>::arg<0>::type arg1) override {\
@@ -57,10 +51,14 @@ class Interface {
     using arg1_type = decltype(arg1);\
     auto iter = g_map<this_type, arg1_type>.map_.find(this);\
     if (iter == g_map<this_type, arg1_type>.map_.end()) {\
-      std::cerr << "Not expected mock function called: "<< getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "Not expected mock function called: " << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
       return;\
     }\
-    auto elem = std::find(std::begin(iter->second), std::end(iter->second), &this_type::_name_);\
+    auto elem = std::find_if(std::begin(iter->second),\
+                             std::end(iter->second),\
+                             [](const auto& pair) -> bool {\
+                               return pair.first == &this_type::_name_;\
+                             });\
     if (elem == iter->second.end()) {\
       std::cerr << "Not expected mock function called:" << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
       return;\
@@ -73,18 +71,23 @@ class Interface {
     using arg1_type = decltype(arg1);\
     auto iter = g_map<this_type, arg1_type>.map_.find(this);\
     if (iter == g_map<this_type, arg1_type>.map_.end()) {\
-      std::vector<decltype(&this_type::_name_)> vec;\
-      vec.push_back(&this_type::_name_);\
+      std::vector<std::pair<decltype(&this_type::_name_), std::string>> vec;\
+      vec.push_back(std::make_pair(&this_type::_name_, getNameForType<this_type>() + "::" + #_name_));\
       g_map<this_type, arg1_type>.map_[this] = vec;\
     } else {\
-      iter->second.push_back(&this_type::_name_);\
+      iter->second.push_back(std::make_pair(&this_type::_name_, getNameForType<this_type>() + "::" + #_name_));\
     }\
   }
 
 
+class Interface {
+ public:
+  virtual void foo(int) = 0;
+  virtual void foo2(double) = 0;
+};
+
 class Inh : public Interface {
  public:
-  MOCK_CLASS(Inh);
   MOCK_METHOD1(foo, void(int));
   MOCK_METHOD1(foo2, void(double));
 };
@@ -93,7 +96,6 @@ class Inh : public Interface {
 #define CONC2(s1, s2) CONC(s1, s2)
 #define UNIQUE_NAME CONC2(UN_, __LINE__)
 
-//#define EXPECT_CALL(obj, fun) ExpectCall<decltype(obj), decltype(&decltype(obj)::fun)> UNIQUE_NAME(&obj, &decltype(obj)::fun)
 #define EXPECT_CALL(obj, fun) obj.MOCK_##fun
 
 }  // unnamed namespace
