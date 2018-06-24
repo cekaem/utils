ed9b35c3d72cf1e678bec9f6fa42eb60f46921dc
Cezary Kulakowski <cezary.kulakowski@gmail.com>
2018-06-24 18:39:06 +0200

    Next part of implementation of mocking mechanism.

--- mock.cc
+++ mock.cc
@@ -21,13 +21,26 @@ struct function_traits<std::function<RT(PT...)>> {
   };
 };
 
-template <typename T, typename ARG1>
-std::map<T*, std::vector<void(T::*)(ARG1)>> g_map;
-
 template <typename T>
 std::string getNameForType() {
   return typeid(T).name();
 }
+
+template <typename T, typename ARG1>
+class MockMap {
+ public:
+  std::map<T*, std::vector<void(T::*)(ARG1)>> map_;
+  ~MockMap() {
+    for (const auto& vec : map_) {
+      for (const auto& mock: vec.second) {
+        std::cerr << "Expected call was not called: " << getNameForType<decltype(mock)>() << std::endl;
+      }
+    }
+  }
+};
+
+template <typename T, typename S>
+MockMap<T, S> g_map;
 
 class Interface {
  public:
@@ -35,13 +48,15 @@ class Interface {
   virtual void foo2(double) = 0;
 };
 
+#define MOCK_CLASS(_name_)
+
 #define MOCK_METHOD1(_name_, _type_)\
   function_traits<std::function<_type_>>::result_type _name_(\
       function_traits<std::function<_type_>>::arg<0>::type arg1) override {\
     using this_type = std::remove_reference<decltype(*this)>::type;\
-    using arg1_type = function_traits<std::function<_type_>>::arg<0>::type;\
-    auto iter = g_map<this_type, arg1_type>.find(this);\
-    if (iter == g_map<this_type, arg1_type>.end()) {\
+    using arg1_type = decltype(arg1);\
+    auto iter = g_map<this_type, arg1_type>.map_.find(this);\
+    if (iter == g_map<this_type, arg1_type>.map_.end()) {\
       std::cerr << "Not expected mock function called: "<< getNameForType<this_type>() << "::" << #_name_ << std::endl;\
       return;\
     }\
@@ -51,66 +66,44 @@ class Interface {
       return;\
     }\
     iter->second.erase(elem);\
+  }\
+  \
+  void MOCK_##_name_(function_traits<std::function<_type_>>::arg<0>::type arg1) {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    using arg1_type = decltype(arg1);\
+    auto iter = g_map<this_type, arg1_type>.map_.find(this);\
+    if (iter == g_map<this_type, arg1_type>.map_.end()) {\
+      std::vector<decltype(&this_type::_name_)> vec;\
+      vec.push_back(&this_type::_name_);\
+      g_map<this_type, arg1_type>.map_[this] = vec;\
+    } else {\
+      iter->second.push_back(&this_type::_name_);\
+    }\
   }
 
 
 class Inh : public Interface {
  public:
+  MOCK_CLASS(Inh);
   MOCK_METHOD1(foo, void(int));
   MOCK_METHOD1(foo2, void(double));
-};
-
-template<typename T, typename F>
-class ExpectCall {
- public:
-  ExpectCall(T* object, F funptr) : object_(object), funptr_(funptr) {
-    //using funptr_type = decltype(object->*funptr);
-    using arg1_type = typename function_traits<decltype(*(object->funptr))>::template arg<0>::type;
-    /*
-    auto iter = g_map<T, arg1_type>.find(object);
-    if (iter == g_map<T, arg1_type>.end()) {
-      std::vector<funptr_type> vec;
-      vec.push_back(funptr);
-      g_map<T, arg1_type>[object] = vec;
-    } else {
-      iter->second.push_back(funptr);
-    }
-    */
-  }
-
-  ~ExpectCall() {
-    /*
-    using funptr_type = decltype(funptr_);
-    using arg1_type = function_traits<std::function<funptr_type>>::arg<0>::type;
-    auto iter = g_map<T, arg1_type>.find(object_);
-    assert(iter != g_map<T, arg1_type>.end());
-    for (const auto& ptr : iter->second) {
-      if (ptr == funptr_) {
-        std::cerr << "Expected call was not called: " << getNameForType<F>() << std::endl;
-      }
-    }
-    */
-  }
-
- private:
-  T* object_;
-  F funptr_;
 };
 
 #define CONC(s1, s2) s1 ## s2
 #define CONC2(s1, s2) CONC(s1, s2)
 #define UNIQUE_NAME CONC2(UN_, __LINE__)
 
-#define EXPECT_CALL(obj, fun) ExpectCall<decltype(obj), decltype(&decltype(obj)::fun)> UNIQUE_NAME(&obj, &decltype(obj)::fun)
+//#define EXPECT_CALL(obj, fun) ExpectCall<decltype(obj), decltype(&decltype(obj)::fun)> UNIQUE_NAME(&obj, &decltype(obj)::fun)
+#define EXPECT_CALL(obj, fun) obj.MOCK_##fun
 
 }  // unnamed namespace
 
-
 int main() {
   Inh inh;
-  EXPECT_CALL(inh, foo);
-  EXPECT_CALL(inh, foo2);
-  //inh.foo(1);
-  //inh.foo(2.0);
+  EXPECT_CALL(inh, foo(5));
+  EXPECT_CALL(inh, foo2(7.0));
+  inh.foo(1);
+  inh.foo2(2.0);
+
   return 0;
 }
