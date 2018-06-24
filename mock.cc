aea3cceca70692ad531a3225c3a096696f351dff
Cezary Kulakowski <cezary.kulakowski@gmail.com>
2018-06-24 21:26:40 +0200

    Next part of implementation of mocking mechanism.

--- mock.cc
+++ mock.cc
@@ -4,6 +4,7 @@
 #include <functional>
 #include <map>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -28,21 +29,63 @@ std::string getNameForType() {
   return typeid(T).name();
 }
 
-template <typename T, typename ARG1>
+template <typename T, typename ...ARGS>
 class MockMap {
  public:
-  std::map<T*, std::vector<std::pair<void(T::*)(ARG1), std::string>>> map_;
+  std::map<T*, std::vector<std::tuple<std::string, void(T::*)(ARGS...), ARGS...>>> map_;
   ~MockMap() {
-    for (const auto& vec : map_) {
-      for (const auto& pair: vec.second) {
-        std::cerr << "Expected call was not called: " << pair.second << std::endl;
+    for (const auto& elem : map_) {
+      for (const auto& tuple: elem.second) {
+        std::cerr << "Expected call was not called: " << std::get<0>(tuple) << std::endl;
+        if constexpr(sizeof...(ARGS) > 0) {
+          std::cerr << "with values: " << std::get<2>(tuple);
+        }
+        if constexpr(sizeof...(ARGS) > 1) {
+          std::cerr << " ; " << std::get<3>(tuple);
+        }
+        if constexpr(sizeof...(ARGS) > 2) {
+          std::cerr << " ; " << std::get<4>(tuple);
+        }
+        std::cerr << std::endl << std::endl;
       }
     }
   }
 };
 
-template <typename T, typename S>
-MockMap<T, S> g_map;
+template <typename T, typename ...ARGS>
+MockMap<T, ARGS...> g_map;
+
+#define MOCK_METHOD0(_name_, _type_)\
+  function_traits<std::function<_type_>>::result_type _name_() override {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    auto iter = g_map<this_type>.map_.find(this);\
+    if (iter == g_map<this_type>.map_.end()) {\
+      std::cerr << "Not expected mock function called: " << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      return;\
+    }\
+    auto elem = std::find_if(std::begin(iter->second),\
+                             std::end(iter->second),\
+                             [](const auto& tuple) -> bool {\
+                               return std::get<1>(tuple) == &this_type::_name_;\
+                             });\
+    if (elem == iter->second.end()) {\
+      std::cerr << "Not expected mock function called:" << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      return;\
+    }\
+    iter->second.erase(elem);\
+  }\
+  \
+  void MOCK_##_name_() {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    auto iter = g_map<this_type>.map_.find(this);\
+    if (iter == g_map<this_type>.map_.end()) {\
+      std::vector<std::tuple<std::string, decltype(&this_type::_name_)>> vec;\
+      vec.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_));\
+      g_map<this_type>.map_[this] = vec;\
+    } else {\
+      iter->second.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_));\
+    }\
+  }
 
 #define MOCK_METHOD1(_name_, _type_)\
   function_traits<std::function<_type_>>::result_type _name_(\
@@ -52,15 +95,18 @@ MockMap<T, S> g_map;
     auto iter = g_map<this_type, arg1_type>.map_.find(this);\
     if (iter == g_map<this_type, arg1_type>.map_.end()) {\
       std::cerr << "Not expected mock function called: " << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "with value: " << arg1 << std::endl << std::endl;\
       return;\
     }\
     auto elem = std::find_if(std::begin(iter->second),\
                              std::end(iter->second),\
-                             [](const auto& pair) -> bool {\
-                               return pair.first == &this_type::_name_;\
+                             [arg1](const auto& tuple) -> bool {\
+                               return std::get<1>(tuple) == &this_type::_name_ &&\
+                                      std::get<2>(tuple) == arg1;\
                              });\
     if (elem == iter->second.end()) {\
       std::cerr << "Not expected mock function called:" << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "with value: " << arg1 << std::endl << std::endl;\
       return;\
     }\
     iter->second.erase(elem);\
@@ -71,30 +117,123 @@ MockMap<T, S> g_map;
     using arg1_type = decltype(arg1);\
     auto iter = g_map<this_type, arg1_type>.map_.find(this);\
     if (iter == g_map<this_type, arg1_type>.map_.end()) {\
-      std::vector<std::pair<decltype(&this_type::_name_), std::string>> vec;\
-      vec.push_back(std::make_pair(&this_type::_name_, getNameForType<this_type>() + "::" + #_name_));\
+      std::vector<std::tuple<std::string, decltype(&this_type::_name_), arg1_type>> vec;\
+      vec.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_, arg1));\
       g_map<this_type, arg1_type>.map_[this] = vec;\
     } else {\
-      iter->second.push_back(std::make_pair(&this_type::_name_, getNameForType<this_type>() + "::" + #_name_));\
+      iter->second.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_, arg1));\
+    }\
+  }
+
+#define MOCK_METHOD2(_name_, _type_)\
+  function_traits<std::function<_type_>>::result_type _name_(\
+      function_traits<std::function<_type_>>::arg<0>::type arg1,\
+      function_traits<std::function<_type_>>::arg<1>::type arg2) override {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    using arg1_type = decltype(arg1);\
+    using arg2_type = decltype(arg2);\
+    auto iter = g_map<this_type, arg1_type, arg2_type>.map_.find(this);\
+    if (iter == g_map<this_type, arg1_type, arg2_type>.map_.end()) {\
+      std::cerr << "Not expected mock function called: " << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "with values: " << arg1 << " ; " << arg2 << std::endl << std::endl;\
+      return;\
+    }\
+    auto elem = std::find_if(std::begin(iter->second),\
+                             std::end(iter->second),\
+                             [arg1, arg2](const auto& tuple) -> bool {\
+                               return std::get<1>(tuple) == &this_type::_name_ &&\
+                                      std::get<2>(tuple) == arg1 &&\
+                                      std::get<3>(tuple) == arg2;\
+                             });\
+    if (elem == iter->second.end()) {\
+      std::cerr << "Not expected mock function called:" << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "with values: " << arg1 << " ; " << arg2 << std::endl << std::endl;\
+      return;\
+    }\
+    iter->second.erase(elem);\
+  }\
+  \
+  void MOCK_##_name_(function_traits<std::function<_type_>>::arg<0>::type arg1,\
+                     function_traits<std::function<_type_>>::arg<1>::type arg2) {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    using arg1_type = decltype(arg1);\
+    using arg2_type = decltype(arg2);\
+    auto iter = g_map<this_type, arg1_type, arg2_type>.map_.find(this);\
+    if (iter == g_map<this_type, arg1_type, arg2_type>.map_.end()) {\
+      std::vector<std::tuple<std::string, decltype(&this_type::_name_), arg1_type, arg2_type>> vec;\
+      vec.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_, arg1, arg2));\
+      g_map<this_type, arg1_type, arg2_type>.map_[this] = vec;\
+    } else {\
+      iter->second.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_, arg1, arg2));\
+    }\
+  }
+
+#define MOCK_METHOD3(_name_, _type_)\
+  function_traits<std::function<_type_>>::result_type _name_(\
+      function_traits<std::function<_type_>>::arg<0>::type arg1,\
+      function_traits<std::function<_type_>>::arg<1>::type arg2,\
+      function_traits<std::function<_type_>>::arg<2>::type arg3) override {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    using arg1_type = decltype(arg1);\
+    using arg2_type = decltype(arg2);\
+    using arg3_type = decltype(arg3);\
+    auto iter = g_map<this_type, arg1_type, arg2_type, arg3_type>.map_.find(this);\
+    if (iter == g_map<this_type, arg1_type, arg2_type, arg3_type>.map_.end()) {\
+      std::cerr << "Not expected mock function called: " << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "with values: " << arg1 << " ; " << arg2 << ", " << arg3 << std::endl << std::endl;\
+      return;\
+    }\
+    auto elem = std::find_if(std::begin(iter->second),\
+                             std::end(iter->second),\
+                             [arg1, arg2, arg3](const auto& tuple) -> bool {\
+                               return std::get<1>(tuple) == &this_type::_name_ &&\
+                                      std::get<2>(tuple) == arg1 &&\
+                                      std::get<3>(tuple) == arg2 &&\
+                                      std::get<4>(tuple) == arg3;\
+                             });\
+    if (elem == iter->second.end()) {\
+      std::cerr << "Not expected mock function called:" << getNameForType<this_type>() << "::" << #_name_ << std::endl;\
+      std::cerr << "with values: " << arg1 << " ; " << arg2 << " ; " << arg3 << std::endl << std::endl;\
+      return;\
+    }\
+    iter->second.erase(elem);\
+  }\
+  \
+  void MOCK_##_name_(function_traits<std::function<_type_>>::arg<0>::type arg1,\
+                     function_traits<std::function<_type_>>::arg<1>::type arg2,\
+                     function_traits<std::function<_type_>>::arg<2>::type arg3) {\
+    using this_type = std::remove_reference<decltype(*this)>::type;\
+    using arg1_type = decltype(arg1);\
+    using arg2_type = decltype(arg2);\
+    using arg3_type = decltype(arg3);\
+    auto iter = g_map<this_type, arg1_type, arg2_type, arg3_type>.map_.find(this);\
+    if (iter == g_map<this_type, arg1_type, arg2_type, arg3_type>.map_.end()) {\
+      std::vector<std::tuple<std::string, decltype(&this_type::_name_), arg1_type, arg2_type, arg3_type>> vec;\
+      vec.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_, arg1, arg2, arg3));\
+      g_map<this_type, arg1_type, arg2_type, arg3_type>.map_[this] = vec;\
+    } else {\
+      iter->second.push_back(std::make_tuple(getNameForType<this_type>() + "::" + #_name_, &this_type::_name_, arg1, arg2, arg3));\
     }\
   }
 
 
 class Interface {
  public:
-  virtual void foo(int) = 0;
-  virtual void foo2(double) = 0;
+  virtual void foo1() = 0;
+  virtual void foo2(int) = 0;
+  virtual void foo3(double) = 0;
+  virtual void foo4(int, double) = 0;
+  virtual void foo5(int, double, std::string) = 0;
 };
 
 class Inh : public Interface {
  public:
-  MOCK_METHOD1(foo, void(int));
-  MOCK_METHOD1(foo2, void(double));
+  MOCK_METHOD0(foo1, void());
+  MOCK_METHOD1(foo2, void(int));
+  MOCK_METHOD1(foo3, void(double));
+  MOCK_METHOD2(foo4, void(int, double));
+  MOCK_METHOD3(foo5, void(int, double, std::string));
 };
-
-#define CONC(s1, s2) s1 ## s2
-#define CONC2(s1, s2) CONC(s1, s2)
-#define UNIQUE_NAME CONC2(UN_, __LINE__)
 
 #define EXPECT_CALL(obj, fun) obj.MOCK_##fun
 
@@ -102,10 +241,16 @@ class Inh : public Interface {
 
 int main() {
   Inh inh;
-  EXPECT_CALL(inh, foo(5));
-  EXPECT_CALL(inh, foo2(7.0));
-  inh.foo(1);
-  inh.foo2(2.0);
+  EXPECT_CALL(inh, foo1());
+  EXPECT_CALL(inh, foo2(1));
+  EXPECT_CALL(inh, foo3(2.0));
+  EXPECT_CALL(inh, foo4(5, 3.0));
+  EXPECT_CALL(inh, foo5(7, 8.0, "string"));
+  inh.foo1();
+  inh.foo2(1);
+  inh.foo3(2.0);
+  inh.foo4(5, 3.0);
+  inh.foo5(7, 8.0, "string");
 
   return 0;
 }
